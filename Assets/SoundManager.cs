095769d4eaf235156655fca33dbcfd23c14a93f4
AlexS64 <elallp123@gmail.com>
2017-11-04 23:59:06 +0100

    Finished sound Manager

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public bool playSound = true;
+
+    private void Update()
+    {
+        if (!playSound && !GetComponent<AudioSource>().mute)
+        {
+            GetComponent<AudioSource>().mute = true;
+        }
+
+        if (playSound && GetComponent<AudioSource>().mute)
+        {
+            GetComponent<AudioSource>().mute = false;
+        }
+    }
+
+    public void ChangeMute()
+    {
+        if (playSound)
+        {
+            playSound = false;
+        }
+        else
+        {
+            playSound = true;
+        }
+    }
+}
