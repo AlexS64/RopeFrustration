ed2e61927f6c232f5d0cdd8c85a767271ef04dff
Tristan Kobusch <tristan.kobusch@gmail.com>
2017-11-05 02:50:37 +0100

    update stick

--- Assets/StickScript.cs
+++ Assets/StickScript.cs
@@ -8,13 +8,35 @@ public class StickScript : MonoBehaviour {
     public GameObject orange;
     public GameObject green;
 
-	// Use this for initialization
-	void Start () {
-		
+    public static float height = 70;
+    public static float decrement = 0.5f;
+
+    private float greenWidth;
+    private float orangeWidth;
+
+	void Start ()
+    {
+        greenWidth = green.transform.localScale.x;
+        orangeWidth = orange.transform.localScale.x;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		red.GetComponent<>
-	}
+	void Update ()
+    {
+        //update width
+        //update objects
+        green.transform.localScale = new Vector3(greenWidth, height, 1);
+        orange.transform.localScale = new Vector3(orangeWidth, height, 1);
+        //update pointer
+
+    }
+
+    void updateWidth()
+    {
+        green.
+    }
+
+    public void move()
+    {
+
+    }
 }
